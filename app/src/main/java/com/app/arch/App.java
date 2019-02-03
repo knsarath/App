7f2f6672740bbb119b21ff168f959b056e9dae31
sarath.kn <sarath.kn@robosoftin.com>
2019-02-03 20:41:26 +0530

    added Application class

---
+++ app/src/main/java/com/app/arch/App.java
@@ -0,0 +1,11 @@
+package com.app.arch;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
